3c522a93e8e2d70b3aa990984bcbebd9ea617a0a
idriss <idrissmouliom@gmail.com>
2023-10-11 22:31:36 +0200

    Displaying platform icons

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import axios from "axios";
-import { Text } from "@chakra-ui/react";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
